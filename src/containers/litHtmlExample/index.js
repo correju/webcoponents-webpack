3a7fee965abbf767d82364ec3b24729a0dacad97
Julian Correa <julian@torre.co>
2018-10-29 17:05:56 -0500

    event handler

--- src/containers/litHtmlExample/index.js
+++ src/containers/litHtmlExample/index.js
@@ -5,21 +5,28 @@ import css from './style.pcss';
 export default class LiHtmlTest extends PolymerElement {
   constructor() {
     super();
-    let array = ['Foo', 'Bar', 'Baz'];
-    const mountPoint = document.createElement('span');
+    this.array = ['Foo', 'Bar', 'Baz'];
+    this.mountPoint = document.createElement('span');
     const shadowRoot = this.attachShadow({ mode: 'open' });
     shadowRoot.innerHTML = `<style>${css}</style>`;
-    shadowRoot.appendChild(mountPoint);
-    const helloTemplate = (names) => html`
+    shadowRoot.appendChild(this.mountPoint);
+    this.helloTemplate = (names) => html`
       <ul>
         ${names.map(e => html`<li class="list-style">lit-html ${e}!</li>`)}
       </ul>
+      <div class="btn-container">
+        <button class="btn button-style" @click="${(event) => this._click()}">HAGA CLICK</button>
+      </div>
     `;
-    setTimeout(() => {
-      render(helloTemplate([...array, 'Test']), mountPoint);
-    }, 2000);
-    render(helloTemplate(array), mountPoint);
+    // setTimeout(() => {
+    //   render(this.helloTemplate([...array, 'Test']), this.mountPoint);
+    // }, 2000);
+    render(this.helloTemplate(this.array), this.mountPoint);
 
+  }
+  _click () {
+    this.array = [...this.array, 'Test'];
+    render(this.helloTemplate(this.array), this.mountPoint);
   }
   static get observedAttributes() {
       return [];
